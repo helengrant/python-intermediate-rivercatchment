2700e59dbecc7e0d0e583e248082cb7e12cb7408
Helen Grant <helen.grant@manchester.ac.uk>
2023-03-30 11:33:37 +0100

    Copy Doug's models and test_models

--- catchment/models.py
+++ catchment/models.py
@@ -9,8 +9,6 @@ time across all sites.
 
 import pandas as pd
 import numpy as np
-from functools import reduce
-
 
 def read_variable_from_csv(filename):
     """Reads a named variable from a CSV file, and returns a
@@ -24,8 +22,8 @@ def read_variable_from_csv(filename):
     """
     dataset = pd.read_csv(filename, usecols=['Date', 'Site', 'Rainfall (mm)'])
 
-    dataset = dataset.rename({'Date': 'OldDate'}, axis='columns')
-    dataset['Date'] = [pd.to_datetime(x, dayfirst=True) for x in dataset['OldDate']]
+    dataset = dataset.rename({'Date':'OldDate'}, axis='columns')
+    dataset['Date'] = [pd.to_datetime(x,dayfirst=True) for x in dataset['OldDate']]
     dataset = dataset.drop('OldDate', axis='columns')
 
     newdataset = pd.DataFrame(index=dataset['Date'].unique())
@@ -37,30 +35,32 @@ def read_variable_from_csv(filename):
 
     return newdataset
 
-
 def daily_total(data):
     """Calculate the daily total of a 2D data array.
 
-    :param data: A 2D pandas dataframe with measurable data.
+    :param data: A 2D Pandas data frame with measurement data.
                  Index must be np.datetime64 compatible format. Columns are measurement sites.
-    :returns: A 2D pandas dataframe with total values of measurement for each day."""
+    :returns: A 2D Pandas data frame with total values of the measurements for each day.
+    """
     return data.groupby(data.index.date).sum()
 
-
 def daily_mean(data):
-    """Calculate the daily mean of a 2d data array.
-    :param data: A 2D pandas dataframe with measurable data.
+    """Calculate the daily mean of a 2D data array.
+
+    :param data: A 2D Pandas data frame with measurement data.
                  Index must be np.datetime64 compatible format. Columns are measurement sites.
-    :returns: A 2D pandas dataframe with mean values of measurement for each day."""
+    :returns: A 2D Pandas data frame with mean values of the measurements for each day.
+    """
     return data.groupby(data.index.date).mean()
 
 
 def daily_max(data):
-    """Calculate the daily minimum of a 2D data array.
+    """Calculate the daily maximum of a 2D data array.
 
     :param data: A 2D Pandas data frame with measurement data.
                  Index must be np.datetime64 compatible format. Columns are measurement sites.
-    :returns: A 2D Pandas data frame with maximum values of the measurements for each day."""
+    :returns: A 2D Pandas data frame with maximum values of the measurements for each day.
+    """
     return data.groupby(data.index.date).max()
 
 
@@ -69,80 +69,79 @@ def daily_min(data):
 
     :param data: A 2D Pandas data frame with measurement data.
                  Index must be np.datetime64 compatible format. Columns are measurement sites.
-    :returns: A 2D Pandas data frame with minimum values of the measurements for each day."""
+    :returns: A 2D Pandas data frame with minimum values of the measurements for each day.
+    """
     return data.groupby(data.index.date).min()
 
-
 def data_normalise(data):
-    """Normalise any given 2D array"""
-    max_array = np.array(np.max(data, axis=0))
-    return data / max_array[np.newaxis, :]
+    """Calculate the normalised values for each column in a given 2D array.
+    Range will be 0-1. But negative values are not screened out. And NaNs
+    in numpy arrays are not screened out either.
+
+    :param data: A 2D data array (numpy or pandas) with measurement data.
+    :returns: A 2D data array, of the same type as the input data array
+              with measurements normalised.
+    """
+    max = np.array(np.max(data, axis=0))
+    return data / max[np.newaxis, :]
 
 
-def data_above_threshold(site_id, data, threshold):
-    """Determine whether measurement value is above given threshold and count them"""
-    def count_above_threshold(a, b):
-        if b:
-            return a+1
-        else:
-            return a
-    above_threshold = (map(lambda x: x > threshold, data[site_id]))
-    return reduce(count_above_threshold, above_threshold, 0)
-
-
-class MeasurementSeries: # A new class separate from sites that contains the data
+class MeasurementSeries:
     def __init__(self, series, name, units):
-        self.series = series # This will be the data
-        self.name = name # This will be the name
-        self.units = units # This will be the units of the data
+        self.series = series
+        self.name = name
+        self.units = units
         self.series.name = self.name
 
     def add_measurement(self, data):
         self.series = pd.concat([self.series, data])
         self.series.name = self.name
 
-    def __str__(self): # Make sure printing shows strings rather than data types
+    def __str__(self):
         if self.units:
             return f"{self.name} ({self.units})"
         else:
             return self.name
 
 
-class Location: # A little class of just location name
+class Location:
     def __init__(self, name):
         self.name = name
+
     def __str__(self):
         return self.name
 
-class Site(Location): # Creating a 'site' class that inherits information from the Location
-    """A measurement site in the study."""
-    version = 0.1
 
+class Site(Location):
     def __init__(self, name):
-        super().__init__(name) # Because we are inheriting a name from the Location class
+        super().__init__(name)
         self.measurements = {}
 
     def add_measurement(self, measurement_id, data, units=None):
         if measurement_id in self.measurements.keys():
             self.measurements[measurement_id].add_measurement(data)
+
         else:
             self.measurements[measurement_id] = MeasurementSeries(data, measurement_id, units)
 
-    @classmethod
-    def get_version(cls): # This means you can print the version as a string
-        return "version "+str(cls.version)
-
-    @staticmethod
-    def create_sample_site():
-        return Site("sample")
-
-    def __str__(self): # this is so that if you print name it gives a string not the representation
-        return self.name
-
-    @property # You can pretend a function looks like an attribute in your class
-    def last_measurements(self): # Essentially a function with no arguments/inputs
+    @property
+    def last_measurements(self):
         return pd.concat(
-            [self.measurements[key][-1:]
-             for key in self.measurements.keys()], axis=1).sort_index()
+            [self.measurements[key].series[-1:] for key in self.measurements.keys()],
+            axis=1).sort_index()
+
+class Catchment(Location):
+    """A catchment area in the study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.sites = {}
 
 
+    def add_site(self, new_site):
+        # Basic check to see if the site has already been added to the catchment area
+        for site in self.sites:
+            if site == new_site:
+                print(f'{new_site} has already been added to site list')
+                return
+
+        self.sites[new_site.name] = Site(new_site)
